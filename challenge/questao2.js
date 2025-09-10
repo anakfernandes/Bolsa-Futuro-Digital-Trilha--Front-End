2ff3757866cf5eb43c642a14739c8d13f5f3a5d7
Ana Fernandes <118865016+anakfernandes@users.noreply.github.com>
2025-09-10 19:06:40 -0300

    Delete challenge directory

--- challenge/questao2.js
+++
@@ -1,8 +0,0 @@
-const prompt = require('prompt-sync')();
-
-let numero = parseInt(prompt("Digite um número para ver a tabuada: "));
-
-console.log("Tabuada do " + numero + ":");
-for (let i = 1; i <= 10; i++) {
-    console.log(numero + " x " + i + " = " + (numero * i));
-}
